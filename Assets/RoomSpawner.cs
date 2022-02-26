041cd6f65fac86d9f067b69e02f41837e784bd48
jbedette <john.bedette45@gmail.com>
2022-02-25 17:11:46 -0800

    Maze is building and navigable

--- Assets/RoomSpawner.cs
+++ Assets/RoomSpawner.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class RoomSpawner : MonoBehaviour
 {
     public int openingDirection;
+    public bool spawned;
     //1 North
     //2 South
     //3 East
@@ -12,7 +12,6 @@ public class RoomSpawner : MonoBehaviour
         
     private RoomTemplates templates;
     private int rand;
-    private bool spawned;
 
 void Start(){
     // Debug.Log("Start");
@@ -22,38 +21,51 @@ void Start(){
 }
 
     void Spawn(){
-        // Debug.Log("Spawn, od " + openingDirection);
+        // Debug.Log(this.transform.parent.parent.parent + ", Spawn #"+ count);
         if(spawned == false){
 
             if(openingDirection == 1){
                 rand = Random.Range(0, templates.northRooms.Length);
-                // Debug.Log("N Rand " + rand);
+                // debugHelper('n');
                 Instantiate(templates.northRooms[rand],transform.position,templates.northRooms[rand].transform.rotation);
+                // Debug.Log(this.transform.parent.parent.parent + " at "+ this.transform.position.x + ","+this.transform.position.z+ " made n room");
             }
             else if(openingDirection ==2){
                 rand = Random.Range(0, templates.southRooms.Length);
-                // Debug.Log("S Rand " + rand);
+                // debugHelper('s');
                 Instantiate(templates.southRooms[rand],transform.position,templates.southRooms[rand].transform.rotation);
 
+                // Debug.Log(this.transform.parent.parent.parent + " at "+ this.transform.position.x + ","+this.transform.position.z+ " made s room");
             }else if(openingDirection ==3){
                 rand = Random.Range(0, templates.eastRooms.Length);
-                // Debug.Log("E Rand " + rand);
+                // debugHelper('e');
                 Instantiate(templates.eastRooms[rand],transform.position,templates.eastRooms[rand].transform.rotation);
 
+                // Debug.Log(this.transform.parent.parent.parent + " at "+ this.transform.position.x + ","+this.transform.position.z+ " made e room");
             }else if(openingDirection ==4){
                 rand = Random.Range(0, templates.westRooms.Length);
-                // Debug.Log("W Rand " + rand);
+                // debugHelper('w');
                 Instantiate(templates.westRooms[rand],transform.position,templates.westRooms[rand].transform.rotation);
+                // Debug.Log(this.transform.parent.parent.parent + " at "+ this.transform.position.x + ","+this.transform.position.z+" made w room");
 
             }
             spawned = true;
         }
+        else  {
+                Debug.Log(this.transform.parent.parent.parent + " at "+ this.transform.position.x + ","+this.transform.position.z+" made w room");
+        }
 
     }
     void OnTriggerEnter(Collider other){
-        if(other.CompareTag("SpawnPoint") || other.CompareTag("Boundary") || other.CompareTag("WallCheck")){
-            Debug.Log(other.tag);
+        if((other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawner>().spawned==true)|| other.CompareTag("Wall")){
+            //Debug.Log(other.tag);
+            debugHelper('x');
             Destroy(gameObject);
         }
     }
+    void debugHelper(char letter){
+        // Debug.Log(this.transform.parent.parent.parent + " at "+ this.transform.position.x + ","+this.transform.position.z +", Spawn #" + count + ": "+ letter +" Rand " + rand);
+        Debug.Log(this.transform.parent.parent.parent + " at "+ this.transform.position.x + ","+this.transform.position.z);
+    }
+
 }
