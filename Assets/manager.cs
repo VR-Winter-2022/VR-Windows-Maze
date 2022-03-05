5644bfe3cafdc4244aeed2335981f2ec0793405f
Gabrielle George <gageorge@pdx.edu>
2022-03-04 23:23:16 -0800

    added controller models and fixed scaling

---
+++ Assets/manager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manager : MonoBehaviour
+{
+    public bool spawn_finished;
+    // Update is called once per frame
+}
---
+++ Assets/teleport.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class teleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TeleportationArea areas = GameObject.FindGameObjectWithTag("Terrain").AddComponent<TeleportationArea>() as TeleportationArea;
+        UnityEngine.GameObject[] terrains = GameObject.FindGameObjectsWithTag("Terrain");
+        int countdown = 0;
+        while (terrains.Length <100 &&  countdown <400){
+            countdown = countdown+1;
+            terrains = GameObject.FindGameObjectsWithTag("Terrain");
+        }
+        if (countdown >= 400){
+            Debug.Log("getting terrains failed.");
+        }
+        Debug.Log("teleport  terrains:");
+        Debug.Log(terrains.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
